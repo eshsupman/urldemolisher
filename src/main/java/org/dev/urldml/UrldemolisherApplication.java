e3ee4898d0a27874d56c422686443b3f4d6e4c2d
ars <eshsupman@gmail.com>
2026-03-26 19:39:35 +0300

    init commit

---
+++ src/main/java/org/dev/urldml/UrldemolisherApplication.java
@@ -0,0 +1,13 @@
+package org.dev.urldml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrldemolisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrldemolisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev/urldml/UrldemolisherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.urldml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrldemolisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
